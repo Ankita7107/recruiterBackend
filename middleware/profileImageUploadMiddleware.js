e1d157d2a66c41a92eb310681fcf8afb662f8ab5
Ankita7107 <ankita.gholap@technfest.com>
2026-06-08 11:48:15 +0530

    image upload size increased

--- middleware/profileImageUploadMiddleware.js
+++ middleware/profileImageUploadMiddleware.js
@@ -35,7 +35,7 @@ const fileFilter = (req, file, cb) => {
 const profileImageUpload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 2 * 1024 * 1024 } // 2MB max
+  limits: { fileSize: 5 * 1024 * 1024 } // 5MB max
 });
 
 module.exports = profileImageUpload;
